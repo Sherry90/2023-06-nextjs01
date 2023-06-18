970437c0819d17f733484e91810fd96e03b9fa07
Sherry90 <hhlsdkm@gmail.com>
2023-06-18 20:16:42 +0900

    dynamic route

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 const ClientsPage = () => {
+  const clients = [
+    { id: "cask", name: "Sherry" },
+    { id: "wine", name: "Shiraz" },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li>
+            <Link href={`/clients/${client.id}`} key={client.id}>
+              {client.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
