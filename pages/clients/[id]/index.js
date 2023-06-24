46f4fbb8216d5b7abbb7d6c35997da1a9b870242
Sherry90 <hhlsdkm@gmail.com>
2023-06-24 19:04:41 +0900

    프로그래밍 방식으로 네비게이팅 하기

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -3,9 +3,18 @@ import { useRouter } from "next/router";
 const ClientProjectPage = () => {
   const router = useRouter();
   console.log(router.query);
+
+  const loadProjectHandler = () => {
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: 'cask', clientprojectid: 'projecta' },
+    });
+  };
+
   return (
     <div>
       <h1>The Project of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
